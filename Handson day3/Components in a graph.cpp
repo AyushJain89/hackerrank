a153dfba1136f047e183bdb1a61473739388d34c
Ayush Jain <63844331+AyushJain89@users.noreply.github.com>
2020-05-15 08:22:54 +0530

    Update Components in a graph.cpp

--- Handson day3/Components in a graph.cpp
+++ Handson day3/Components in a graph.cpp
@@ -1,93 +1,50 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-using namespace std;
+import java.io.*;
+import java.util.*;
 
-
-struct Node {
-    int data;
-};
-
-struct BinaryNode : Node {
-    BinaryNode *left = NULL;
-    BinaryNode *right = NULL;
-};
-
-struct DisjointSetNode : Node {
-    DisjointSetNode *parent;
-    int rank = 0;
-    int size = 1;
-};
-
-DisjointSetNode *makeSet(int value) {
-    DisjointSetNode *node = new DisjointSetNode;
-    node->data = value;
-    node->parent = node;
-
-    return node;
-}
-
-DisjointSetNode *findSet(DisjointSetNode *node) {
-    if (node->parent == node) { return node; }
-    node->parent = findSet(node->parent);
-    return node->parent;
-}
-
-DisjointSetNode *unionSets(DisjointSetNode *firstNode, DisjointSetNode *secondNode) {
-    DisjointSetNode *firstNodeParent = findSet(firstNode);
-    DisjointSetNode *secondNodeParent = findSet(secondNode);
-
-    if (firstNodeParent == secondNodeParent) {
-        return firstNodeParent;
-    }
-
-    if (firstNodeParent->rank > secondNodeParent->rank) {
-        secondNodeParent->parent = firstNodeParent;
-        firstNodeParent->size += secondNodeParent->size;
-        return firstNodeParent;
-    }
-
-    firstNodeParent->parent = secondNodeParent;
-    secondNodeParent->size += firstNodeParent->size;
-    if (firstNodeParent->rank == secondNodeParent->rank) {
-        secondNodeParent->rank += 1;
-    }
-
-    return secondNodeParent;
-}
-
-int size(DisjointSetNode* node) {
-    DisjointSetNode* parent = findSet(node);
-    return parent->size;
-}
-
-int main() {
-    int N;
-    cin >> N;
-    int MIN = N + 1;
-    int MAX = 2;
-    vector<DisjointSetNode*> nodes(2*N);
-    for (int i = 0; i <= 2*N; i++) {
-        nodes[i] = makeSet(i);
-    }
-    for (int i = 0; i < N; i++) {
-        int edge1, edge2;
-        cin >> edge1 >> edge2;
-
-        unionSets(nodes[edge1], nodes[edge2]);
-    }
-
-    for (int i = 0; i <= 2 * N; i++) {
-        int nrOfNodesInSet = findSet(nodes[i])->size;
-        if (nrOfNodesInSet > 1) {
-            if (nrOfNodesInSet > MAX) { MAX = nrOfNodesInSet; }
-            if (nrOfNodesInSet < MIN) { MIN = nrOfNodesInSet; }
+public class Solution {
+    static class Graph {
+        HashMap<Integer, ArrayList<Integer>> nodes;
+        Graph (int n) {
+            nodes = new HashMap<>();
+            for (int i = 1; i <= 2*n; i++) {
+                ArrayList<Integer> list = new ArrayList<Integer>();
+                list.add(i);
+                nodes.put(i, list);
+            }
+        }
+        
+        void set_edge(int n1, int n2) {
+            ArrayList<Integer> list1 = nodes.get(n1);
+            ArrayList<Integer> list2 = nodes.get(n2);
+            if (list1 != list2) {
+                list1.addAll(list2);
+                list2.forEach(i -> nodes.put(i, list1));
+            }
+        }
+        
+        void print_answer() {
+            ArrayList<Integer> vertices = new ArrayList<Integer>();
+            
+            for (ArrayList<Integer> list : nodes.values()) {
+                if (list.size() > 1)
+                    vertices.add(list.size());
+                list.clear();
+            }
+            System.out.print(Collections.min(vertices) + " ");
+            System.out.println(Collections.max(vertices));
         }
     }
-
-    cout << MIN << " " << MAX;
-
-    return 0;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        Graph G = new Graph(N);
+        for (int i = 0; i < N; i++) {
+            int n1 = in.nextInt();
+            int n2 = in.nextInt();
+            G.set_edge(n1, n2);
+        }
+        
+        G.print_answer();
+    }
 }
+
